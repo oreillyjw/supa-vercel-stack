6afd7fee941da058023ac9665e09a95277ccacea
Jonathon O <oreillyjw@gmail.com>
2025-11-17 21:18:38 -0500

    fix magic link logic issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,32 @@ import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+
+declare module "@remix-run/node" {
+	// or cloudflare, deno, etc.
+	interface Future {
+		v3_singleFetch: true;
+	}
+}
+
 export default defineConfig({
 	plugins: [
 		remix({
 			presets: [vercelPreset()],
 			ignoredRouteFiles: ["**/.*"],
+			future: {
+				unstable_optimizeDeps: true,
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+				v3_lazyRouteDiscovery: true,
+				v3_singleFetch: true,
+			},
 		}),
 		tsconfigPaths(),
 	],
 	server: {
+		port: 3000,
 		watch: {
 			// Watch Tailwind config for changes
 			ignored: ["!**/tailwind.config.ts"],
