f922166feee8eedfb1f414b8317c9fb5dd6e3ba2
Jonathon O <oreillyjw@gmail.com>
2025-11-25 20:03:10 -0500

    update react router to get ssr working

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,5 +2,6 @@ import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
+	ssr: true,
 	presets: [vercelPreset()],
 } satisfies Config;
